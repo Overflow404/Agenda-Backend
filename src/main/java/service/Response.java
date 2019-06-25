12b86e35425cb52ebc1c0c4bf5c465216e65b29a
Overflow404 <gaspare.armato1@gmail.com>
2019-06-25 12:52:43 +0200

    Refactoring

--- src/main/java/service/Response.java
+++ src/main/java/service/Response.java
@@ -1,20 +1,20 @@
-package service.overlapping;
+package service;
 
 import lombok.Getter;
 import lombok.Setter;
-
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 @Getter
 @Setter
-public class OverlappingResult {
-
+public class Response<T> {
+    //TODO mapping with http status code ande xceptions
     static final String BUSY_SLOT = "This slot is busy!";
     static final String START_AFTER_END = "End time must be greater than start time!";
+    static final String FREE_SLOT = "This slot is free!";
 
     private Result result;
-    private String content;
+    private T content;
     private String failureReason;
 
     public enum Result {
@@ -22,26 +22,26 @@ public class OverlappingResult {
         FAILURE
     }
 
-    private OverlappingResult(String content, Result result, String failureReason) {
+    private Response(T content, Result result, String failureReason) {
         this.content = content;
         this.result = result;
         this.failureReason = failureReason;
     }
 
-    public static OverlappingResult success(String content) {
-        return new OverlappingResult(content, Result.SUCCESS, null);
+    static <T> Response success(T content) {
+        return new Response<T>(content, Result.SUCCESS, null);
     }
 
 
-    static OverlappingResult failure(String reason) {
-        return new OverlappingResult(null, Result.FAILURE, reason);
+    static <T> Response failure(String reason) {
+        return new Response<T>(null, Result.FAILURE, reason);
     }
 
-    boolean success() {
+    public boolean success() {
         return result.equals(Result.SUCCESS);
     }
 
-    public String getContent() {
+    public T getContent() {
         return content;
     }
 
