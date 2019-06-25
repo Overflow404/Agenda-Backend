2f5cdd8b81a80bf33a80bd4a69123aba6e2cffa3
Overflow404 <gaspare.armato1@gmail.com>
2019-06-25 12:51:57 +0200

    Addedd retrieve and retrieveall operations.

--- src/main/java/restful/RestfulBookingService.java
+++ src/main/java/restful/RestfulBookingService.java
@@ -1,7 +1,10 @@
 package restful;
 
-import service.overlapping.OverlappingResult;
-import service.overlapping.OverlappingService;
+import model.booking.Booking;
+import service.Response;
+import service.BookingService;
+import service.OverlappingService;
+import service.RetrieveBookingsService;
 
 import javax.ejb.EJB;
 import javax.ws.rs.*;
@@ -14,15 +17,61 @@ public class RestfulBookingService {
     @EJB
     private OverlappingService overlappingService;
 
+    @EJB
+    private BookingService bookingService;
+
+    @EJB
+    private RetrieveBookingsService retrieveBookingsService;
+
+    //TODO timestamp da fare dentro i service e controllare numberformat exception
     @GET
     @Path("/overlapping")
     @Produces(MediaType.APPLICATION_JSON)
-    public OverlappingResult overlap(@QueryParam("startDate") String startDate,
-                                     @QueryParam("endDate") String endDate) {
+    public Response overlap(@QueryParam("startDate") String startDate,
+                            @QueryParam("endDate") String endDate) {
 
         Timestamp startMillisDate = new Timestamp(Long.valueOf(startDate));
         Timestamp endMillisDate = new Timestamp(Long.valueOf(endDate));
-        return overlappingService.checkIfDatesOverlap(startMillisDate, endMillisDate);
+
+        Response response = overlappingService.checkIfDatesOverlap(startMillisDate, endMillisDate);
+        return response;
+    }
+
+    @GET
+    @Path("/book")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response book(@QueryParam("subject") String subject,
+                         @QueryParam("description") String description,
+                         @QueryParam("startDate") String startDate,
+                         @QueryParam("endDate") String endDate) {
+
+        Timestamp startMillisDate = new Timestamp(Long.valueOf(startDate));
+        Timestamp endMillisDate = new Timestamp(Long.valueOf(endDate));
+
+        Booking booking = new Booking(subject, description, startMillisDate, endMillisDate);
+
+        Response response = bookingService.book(booking);
+        return response;
+    }
+
+    @GET
+    @Path("/retrieve")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response retrieve(@QueryParam("day") String d,
+                         @QueryParam("month") String m,
+                         @QueryParam("year") String y) {
+
+        Response response = retrieveBookingsService.retrieve(d, m, y);
+        return response;
+    }
+
+    @GET
+    @Path("/retrieveall")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response retrieveall() {
+
+        Response response = retrieveBookingsService.retrieveAll();
+        return response;
     }
 
 }
